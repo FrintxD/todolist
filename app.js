be930f62d73a76adf1ca8a11666624b818422f20
FrintxD <Skyinfall02@gmail.com>
2021-11-09 12:37:44 -0300

    Mobile Changes + app.js udpate

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ var checkBox = document.querySelectorAll('.circle');
 var taskContent = document.getElementById('task-input');
 var formTaskContent = document.querySelector('.form');
 var taskControl = 1;
+
 //Cheking checkbox.
+
 checkBox.forEach((element)=>{
     element.addEventListener('click', ()=>{
         element.classList.toggle('check-circle');
     });
 })
+
 
 function getTaskContent(){
     return taskContent.value;
@@ -16,14 +19,18 @@ function getTaskContent(){
 function addingNewTask(){
     let taskContainer = document.querySelector('.new-old-task-container');
 
-    taskContainer.innerHTML +='<div' +' '+ `id="task${taskControl}"` + 'class="task"><div class="content"><div class="circle"><img src="images/icon-check.svg" alt="check-img"></div>' + `<p>${getTaskContent()}</p>` + '</div><img class="close" src="images/icon-cross.svg" width="12vw" alt="close"></div>';
+    taskContainer.insertAdjacentHTML('afterbegin', `<div id="task${taskControl}" class="task"><div class="content"><div class="circle"><img src="images/icon-check.svg" alt="check-img"></div><p>${getTaskContent()}</p></div><img id="task${taskControl}" class="close" src="images/icon-cross.svg" width="12vw" alt="close"></div>`);
 }
 
 function removeTask(){
-    let task = getElementById(`task${taskControl}`);
-    task.removeChild(task);
-}
+    var close = document.querySelectorAll('.close');
+    close.forEach((element)=>{
+        element.addEventListener('click', ()=>{
+            console.log('si');
+        });
+    })
 
+}
 
 function clearinput(){
     if(getTaskContent() != null){
@@ -32,7 +39,7 @@ function clearinput(){
 }
 
 
-formTaskContent.addEventListener('submit', (e)=>{
+formTaskContent.addEventListener('submit',function(e){
     e.preventDefault()
     addingNewTask();
 
@@ -41,3 +48,4 @@ formTaskContent.addEventListener('submit', (e)=>{
     clearinput();
 })
 
+removeTask();
